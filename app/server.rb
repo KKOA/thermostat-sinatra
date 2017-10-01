39106b71401b12eb6eed7ec83621f3c21965a1da
keith(mac) <ka33145@googlemail.com>
2017-10-01 22:47:11 +0100

    Renamed app/app.rb as app/server.rb and class to server

--- app/server.rb
+++ app/server.rb
@@ -3,11 +3,11 @@ ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 # require_relative 'data_mapper_setup'
 
-class App < Sinatra::Base
+class Server < Sinatra::Base
   get '/' do
     @title = 'Thermostat'
     erb(:index)
   end
-
+  
   run! if app_file == $PROGRAM_NAME
 end
