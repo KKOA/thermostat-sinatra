dfc2155e4272aaf2450b6c8f4556ad83cc3ec6ad
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-21 19:34:59 +0000

    Clean server.rb routes

--- app/server.rb
+++ app/server.rb
@@ -7,58 +7,44 @@ require 'json'
 class Server < Sinatra::Base
   get '/' do
     @title = 'Thermostat'
+    @cities = City.all
     erb(:index)
   end
 
   get '/cities' do
     content_type :json
-    @cities = City.all.to_json
-    # content_type :json
-    # @cities.map do |city|
-    #     city.name
-    # end.to_json
+    @cities = City.all.to_json # return all cities as json object
   end
 
-  # post '/save' do
-  #   thermostat = Thermostat.get(1)
-  #
-  #   thermostat.temperature = params[:temperature]
-  #   thermostat.city = params[:city] || thermostat.city
-  #   thermostat.save
-  # end
-
   post '/save' do
-
-
-    if(Thermostat.get(1).nil?)
-      Thermostat.create(:temperature => params[:temperature], :city => params[:city])
+    if Thermostat.get(1).nil?
+      Thermostat.create(
+        temperature: params[:temperature],
+        city: params[:city]
+      )
     else
       @thermostat = Thermostat.get(1)
       @thermostat.temperature = params[:temperature]
       @thermostat.city = params[:city]
+      # Update Selected item in database
+      City.all(:name.not => params[:city]).update(selected: false)
+      City.first(name: params[:city]).update(selected: true)
       @thermostat.save
     end
-
-  end
-
-  get '/x' do
-    puts Thermostat.get(1).nil?
-    # @thermostat.count.zero?
-    # [].zer
   end
 
   get '/get_data' do
-
     @thermostat = Thermostat.get(1)
-
     content_type :json
-    if(Thermostat.get(1).nil?)
-      return {:temperature => '' , :city => ''}.to_json
+    if Thermostat.get(1).nil?
+      return { temperature: '', city: '' }.to_json
     else
       @thermostat = Thermostat.get(1)
-      {:temperature => @thermostat.temperature , :city => @thermostat.city}.to_json
+      {
+        temperature: @thermostat.temperature,
+        city: @thermostat.city
+      }.to_json
     end
   end
-
   run! if app_file == $PROGRAM_NAME
 end
